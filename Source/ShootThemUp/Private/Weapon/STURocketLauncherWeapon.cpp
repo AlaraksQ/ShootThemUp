20a9ba6126f8d064ae397a87150900a594c1ab56
Alaraks <Alaraks999@gmail.com>
2025-09-11 16:52:36 +0300

    Added projectile spawn for launcher

--- Source/ShootThemUp/Private/Weapon/STURocketLauncherWeapon.cpp
+++ Source/ShootThemUp/Private/Weapon/STURocketLauncherWeapon.cpp
@@ -2,4 +2,23 @@
 
 
 #include "Weapon/STURocketLauncherWeapon.h"
+#include "Weapon/STUProjectile.h"
+#include "Kismet/GameplayStatics.h"
 
+void ASTURocketLauncherWeapon::StartFire()
+{
+	MakeShot();
+}
+
+void ASTURocketLauncherWeapon::MakeShot()
+{
+	const FTransform SpawnTransform(FRotator::ZeroRotator, GetMuzzleWorldLocation());
+	auto Projectile = UGameplayStatics::BeginDeferredActorSpawnFromClass(
+		GetWorld(),
+		ProjectileClass,
+		SpawnTransform);
+
+	// TODO: Set projectile params
+
+	UGameplayStatics::FinishSpawningActor(Projectile, SpawnTransform);
+}
--- Source/ShootThemUp/Public/Weapon/STURocketLauncherWeapon.h
+++ Source/ShootThemUp/Public/Weapon/STURocketLauncherWeapon.h
@@ -6,12 +6,19 @@
 #include "Weapon/STUBaseWeapon.h"
 #include "STURocketLauncherWeapon.generated.h"
 
-/**
- * 
- */
+class ASTUProjectile;
+
 UCLASS()
 class SHOOTTHEMUP_API ASTURocketLauncherWeapon : public ASTUBaseWeapon
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void StartFire() override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon")
+	TSubclassOf<ASTUProjectile> ProjectileClass;
+
+	virtual void MakeShot() override;
 };
