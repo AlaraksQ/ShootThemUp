9cf465ad6a29fc61d0f894ffb6861535b17c98d7
Alaraks <Alaraks999@gmail.com>
2025-09-17 00:45:12 +0300

    moved HUD logic to cpp

--- Source/ShootThemUp/Private/UI/STUGameHUD.cpp
+++ Source/ShootThemUp/Private/UI/STUGameHUD.cpp
@@ -2,13 +2,32 @@
 
 
 #include "UI/STUGameHUD.h"
+#include "Engine/Canvas.h"
+#include "Blueprint/UserWidget.h"
 
 void ASTUGameHUD::DrawHUD()
 {
+	Super::DrawHUD();
 
+	DrawCrossHair();
 }
 
 void ASTUGameHUD::DrawCrossHair()
 {
+	const TInterval<float> Center(Canvas->SizeX * 0.5f, Canvas->SizeY * 0.5f);
 
+	const float HalfLineSize = 10.0f;
+	const float LineThickness = 2.0f;
+	const FLinearColor LineColor = FLinearColor::Green;
+}
+
+void ASTUGameHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto PlayerHUDWidget = CreateWidget<UUserWidget>(GetWorld(), PlayerHUDWidgetClass);
+	if (PlayerHUDWidget)
+	{
+		PlayerHUDWidget->AddToViewport();
+	}
 }
